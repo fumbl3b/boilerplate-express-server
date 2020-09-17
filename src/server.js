61ff89446a1a54401df51d05c1922fe6b8283372
Harrison Winkler <harryewinkler@thinkful.com>
2020-09-17 11:57:41 -0400

    initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`
+    Server is listening at http://localhost:${PORT}...
+  `);
+});
